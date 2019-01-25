501f8263bbe56c901c8f5292fe7564ebb3993038
Juutis <jutkula@gmail.com>
2019-01-25 21:41:29 +0200

    Added basic movement implementation and test scene.

---
+++ FGJ-2019/Assets/Scripts/player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float moveSpeed = 20;
+
+    Rigidbody2D rigidbody;
+    
+    Vector2 desiredMoveDirection;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //reading the input:
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        //camera forward and right vectors:
+        var up = Vector2.up;
+        var right = Vector2.right;
+
+        desiredMoveDirection = up * verticalAxis + right * horizontalAxis;
+        rigidbody.velocity = desiredMoveDirection * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
