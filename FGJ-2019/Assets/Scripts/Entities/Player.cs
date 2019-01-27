7949ca09724683af3e8dc25f87480e386ce8a968
Juho Saarelainen <bradurdung@gmail.com>
2019-01-27 12:18:50 +0200

    SortingOrder bubblegum.

--- FGJ-2019/Assets/Scripts/Entities/Player.cs
+++ FGJ-2019/Assets/Scripts/Entities/Player.cs
@@ -61,7 +61,7 @@ public class Player : MonoBehaviour {
         }
 
         updatePlayerPosition();
-        spriteRenderer.sortingOrder = (int)Math.Floor(transform.position.y * 10);
+        spriteRenderer.sortingOrder = -(int)Math.Floor(transform.position.y * 10);
 
     }
 
